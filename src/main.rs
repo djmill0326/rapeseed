61874ed7e09aa4f16b78721c41d29c6264027cb9
Dylan Miller <djmill0326@gmail.com>
2024-01-13 15:36:12 -0800

    oh.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 /* #!/bin/curl http://ehpt:666/haptloader/v1 >> /bin/fish */
 
 trait Scanner {
-    fn scan(self) -> Real;
+    fn scan(self) -> Tree;
 }
 
-impl Scanner for Real {
-    fn scan(self) -> Real {
+impl Scanner for Tree {
+    fn scan(self) -> Tree {
         println!("[haptloader] innit");
         self
     }
 }
 
-impl Into<usize> for Real {
+impl Into<usize> for Tree {
     fn into(self) -> usize {
         self.0
     }
@@ -28,12 +28,12 @@ impl Muxer for X {
 }
 
 #[derive(Copy, Clone, Debug, Default)]
-struct X(Real, Real);
+struct X(Tree, Tree);
 
 #[derive(Copy, Clone, Debug, Default)]
-struct Real(usize);
+struct Tree(usize);
 
-impl Real {
+impl Tree {
     fn scan(&self) -> Self {
         println!("[haptloader/scan] you are have virus! {:?}", self.0);
         self.clone()
@@ -52,10 +52,22 @@ impl Real {
         }
     }
 
-    fn set(mux0: X, mux1: X) {
+    unsafe fn set(mux0: X, mux1: X) {
+        MAP[mux0.1.mux(mux1.0).1.0] = mux1;
+        MAP[mux1.0.mux(mux0.1).0.0] = mux0;
+    }
+
+    fn read(&self, mux: X) -> X {
+        let o = self.get(X(mux.0.mux(mux.1).0, mux.1.mux(mux.0).1));
+        println!("[ehptloader] read: <{:?}, {:?}>", o.0.0, o.1.0);
+        o
+    }
+
+    fn write(&self, x: usize) {
         unsafe {
-            MAP[mux0.1.mux(mux1.0).1.0] = mux1;
-            MAP[mux1.0.mux(mux0.1).0.0] = mux0;
+            let a = MAP[x].0.mux(MAP[x].1);
+            let b = MAP[x].1.mux(MAP[x].0);
+            println!("[haptloader] write: <{:?},{:?},{:?},{:?}>", a.0.0, b.0.0, a.1.0, b.1.0);
         }
     }
 }
@@ -69,8 +81,9 @@ impl From<X> for *const X {
 static mut MAP: Vec<X> = Vec::new();
 
 fn main() -> () {
-    let zero = Real(0);
-    let one = Real(1);
-    unsafe { MAP.push(one.mux(one).0.mux(zero)); }
+    let zero = Tree(0);
+    let one = Tree(1);
+    //zero.write(one.0);
+    one.read(zero.mux(one));
     println!("[haptloader] ehpt");
 }
